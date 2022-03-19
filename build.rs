265bffc2c2a4d63be9d3b095b9afa90be3a2d5ba
spking11 <spking11@foxmail.com>
2022-03-19 23:45:52 +0800

    导入libtracefs

---
+++ build.rs
@@ -0,0 +1,22 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    println!("cargo:rustc-link-lib=tracefs");
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .clang_arg("-I/usr/include/tracefs")
+        .clang_arg("-I/usr/include/traceevent")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ wrapper.h
@@ -0,0 +1,1 @@
+#include <tracefs.h>
